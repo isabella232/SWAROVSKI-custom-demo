af5249b1f26065b346b1bdbad0104593cd7a985f
Hugo Valla <hugo_valla@hotmail.fr>
2021-06-23 17:16:02 +0200

    adding variable for apiKey + appID

--- src/index.js
+++ src/index.js
@@ -3,4 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+//CHANGE ME IF USING NEW INDEX
+window.appID = 'JDBD6EJM33'
+window.key = '0fe54b2e3991d370c91376981aff9d48'
+
 ReactDOM.render(<App />, document.getElementById('root'));
