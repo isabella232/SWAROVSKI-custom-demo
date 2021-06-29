1d6276cd0a9bb627a79f4fa1fa2da85279840884
Alexandre Viretti <alexou73@gmail.com>
2021-06-29 11:43:52 +0200

    feat(customrefinement): add

--- src/Components/Searchage/SearchBox.jsx
+++ src/Components/Searchage/SearchBox.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
-import { connectSearchBox } from 'react-instantsearch-dom';
+import {
+    connectSearchBox,
+    connectCurrentRefinements
+} from 'react-instantsearch-dom';
+
+import uniqBy from 'lodash.uniqby';
 
 const SearchBox = ({ currentRefinement, refine }) => {
     return (
@@ -13,10 +18,61 @@ const SearchBox = ({ currentRefinement, refine }) => {
                     placeholder="Search..."
                 />
             </div>
+            <CustomCurrentRefinements
+                transformItems={items =>
+                    items.filter(item => item.attribute !== 'price')
+                }
+            />
         </div>
     );
 };
 
 const CustomSearchBox = connectSearchBox(SearchBox);
+
+const CurrentRefinements = ({ items, refine }) => {
+    const unique = uniqBy(items, 'currentRefinement');
+
+    return (
+        <ul className="refinement-content">
+            {unique.map(item => (
+                <li className="refinement-item" key={item.label}>
+                    {item.items ? (
+                        <React.Fragment>
+                            <h3>{item.label}</h3>
+                            <ul className="refinement-results">
+                                {item.items.map(nested => (
+                                    <li key={nested.label}>
+                                        <a
+                                            className="refinement-filter"
+                                            href="#"
+                                            onClick={event => {
+                                                event.preventDefault();
+                                                refine(nested.value);
+                                            }}
+                                        >
+                                            {nested.label}
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </React.Fragment>
+                    ) : (
+                        <a
+                            href="#"
+                            onClick={event => {
+                                event.preventDefault();
+                                refine(item.value);
+                            }}
+                        >
+                            {item.label}
+                        </a>
+                    )}
+                </li>
+            ))}
+        </ul>
+    );
+};
+
+const CustomCurrentRefinements = connectCurrentRefinements(CurrentRefinements);
 
 export default CustomSearchBox;
