1f2655814eb867ac3ebcf7235ce74596bbfe4afb
Alexandre Viretti <alexou73@gmail.com>
2021-06-30 16:38:55 +0200

    fix(include): add include method

--- src/Components/Searchage/SearchResult.jsx
+++ src/Components/Searchage/SearchResult.jsx
@@ -37,7 +37,7 @@ const SearchResults = ({
         ) {
             setIsGlobal(true);
             setSearchVisible(true);
-            setToggleIsShow(true);
+            setToggleIsShow(false);
             setNoParams(false);
         }
         if (
